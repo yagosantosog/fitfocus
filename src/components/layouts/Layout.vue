263af84cbc69414158414ed901d84cb1ab83bc7e
Yago Santos <santosyago2005@gmail.com>
2026-01-10 23:32:25 -0300

    Add footer initial markup

--- src/components/layouts/Layout.vue
+++ src/components/layouts/Layout.vue
@@ -5,7 +5,16 @@
   <main>
     <slot />
   </main>
-  <footer></footer>
+  <footer>
+    <small>Created by</small>
+    <a href="https://github.com/yagosantosog/fitfocus" target="_blank">
+      <img
+        src="https://avatars.githubusercontent.com/u/94266179?v=4&size=64"
+        alt="profile picture"
+      />
+      <p>yagosantosog</p>
+    </a>
+  </footer>
 </template>
 
 <style scoped></style>
