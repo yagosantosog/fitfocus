50b8b6ec106db9f9b1d1715f10636273e24713cb
Yago Santos <santosyago2005@gmail.com>
2026-01-11 20:12:20 -0300

    Add conditional rendering to the pages inside the App component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
 import Layout from "./components/layouts/layout.vue";
 import Welcome from "./components/pages/Welcome.vue";
 import Dashboard from "./components/pages/Dashboard.vue";
+import Workout from "./components/pages/Workout.vue";
+
+const selectedDisplay = 0;
 </script>
 
 <template>
   <Layout>
-    <Welcome />
-    <Dashboard />
+    <Welcome v-if="selectedDisplay == 0" />
+    <Dashboard v-if="selectedDisplay == 1" />
+    <Workout v-if="selectedDisplay == 2" />
   </Layout>
 </template>
 
