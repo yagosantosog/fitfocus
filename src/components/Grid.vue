3f34d48f5bf8d71619c9c21a4b9f773b6eeca64c
Yago Santos <santosyago2005@gmail.com>
2026-01-11 20:49:28 -0300

    Add the logic to the button element

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,5 +1,26 @@
-<script setup></script>
+<script setup>
+import { workoutProgram } from "../utils";
+const workoutTypes = ["Push", "Pull", "Legs"];
+</script>
 
-<template></template>
+<template>
+  <section id="grid">
+    <button
+      :key="workoutIdx"
+      v-for="(workout, workoutIdx) in Object.keys(workoutProgram)"
+      class="button plan-card"
+    >
+      <div>
+        <p>
+          Day {{ workoutIdx < 9 ? "0" + (workoutIdx + 1) : workoutIdx + 1 }}
+        </p>
+        <i class="fa-solid fa-dumbbell" v-if="workoutIdx % 3 == 0"></i>
+        <i class="fa-solid fa-weight-hanging" v-if="workoutIdx % 3 == 1"></i>
+        <i class="fa-solid fa-bolt" v-if="workoutIdx % 3 == 2"></i>
+      </div>
+      <h3>{{ workoutTypes[workoutIdx % 3] }}</h3>
+    </button>
+  </section>
+</template>
 
 <style scoped></style>
