22667cc7bb30f33652c63dfabb8035aff706ee09
Yago Santos <santosyago2005@gmail.com>
2026-01-12 23:29:20 -0300

    Add underlay button and portal content division to the portal component

--- src/components/Portal.vue
+++ src/components/Portal.vue
@@ -1,7 +1,19 @@
 <script setup></script>
 
 <template>
-  <Teleport to="#portal"> <section>Test</section></Teleport>
+  <Teleport to="#portal">
+    <div class="portal-container">
+      <div
+        role="button"
+        tabindex="0"
+        @click="() => {}"
+        class="portal-underlay"
+      ></div>
+      <div class="portal-content">
+        <slot />
+      </div>
+    </div>
+  </Teleport>
 </template>
 
 <style scoped></style>
