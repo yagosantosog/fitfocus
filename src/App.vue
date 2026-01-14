505ac2b3da60a9795b9aac15b14a06ba9f824bbd
Yago Santos <santosyago2005@gmail.com>
2026-01-13 23:05:16 -0300

    Add data structure to deal with input values

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,33 @@ import Layout from "./components/layouts/layout.vue";
 import Welcome from "./components/pages/Welcome.vue";
 import Dashboard from "./components/pages/Dashboard.vue";
 import Workout from "./components/pages/Workout.vue";
+import { workoutProgram } from "./utils";
+import { ref } from "vue";
 
-const selectedDisplay = 2;
+const defaultData = {};
+for (let workoutIdx in workoutProgram) {
+  const workoutData = workoutProgram[workoutIdx];
+
+  defaultData[workoutIdx] = {};
+
+  for (let e of workoutData.workout) {
+    defaultData[workoutIdx][e.name] = "";
+  }
+}
+const selectedDisplay = ref(2);
+const data = ref(defaultData);
+const selectedWorkout = ref(-1);
 </script>
 
 <template>
   <Layout>
     <Welcome v-if="selectedDisplay == 0" />
     <Dashboard v-if="selectedDisplay == 1" />
-    <Workout v-if="selectedDisplay == 2" />
+    <Workout
+      :data="data"
+      :selectedWorkout="selectedWorkout"
+      v-if="selectedDisplay == 2"
+    />
   </Layout>
 </template>
 
