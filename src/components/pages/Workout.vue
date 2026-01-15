549d6957aee84466e46f31181b9b961cfab378d1
Yago Santos <santosyago2005@gmail.com>
2026-01-15 20:32:31 -0300

    style: add styles to the line between warmup and workout

--- src/components/pages/Workout.vue
+++ src/components/pages/Workout.vue
@@ -145,6 +145,13 @@ function handleCloseModal() {
   grid-column: span 7 / span 7;
 }
 
+.workout-grid-line {
+  margin: 0.5rem 0;
+  height: 3px;
+  border-radius: 2px;
+  background: var(--background-muted);
+}
+
 .grid-name {
   grid-column: span 3;
   display: flex;
