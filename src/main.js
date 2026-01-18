da3bc548b5ba149db81a9afbc97fe8fadbb3842c
Yago Santos <santosyago2005@gmail.com>
2026-01-17 22:30:05 -0300

    Updates

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import "./fanta.css";
-import { initTheme } from "./theme/themeManager.js";
-import App from "./App.vue";
-
-initTheme();
-createApp(App).mount("#app");
